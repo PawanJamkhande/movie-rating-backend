798d20e5329d853a361c14621502d4989f6886d6
Pawan JR. <pawanjamkhande001@gmail.com>
2026-08-04 16:37:34 +0530

    Update UserRepository.java

--- movie-rating-backend/src/main/java/com/movierating/repository/UserRepository.java
+++ movie-rating-backend/src/main/java/com/movierating/repository/UserRepository.java
@@ -5,7 +5,8 @@ import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.movierating.entity.User;
-
+// JpaRepository consists of 100's of methods that can be inherited using extends keyword
+// the method has two arguments User (what entity we are refeerring to ) and Long (the datatype of the primary key of that entity)
 public interface UserRepository extends JpaRepository<User, Long> {
 	// Spring Data JPA auto-implements these based on method name - no manual query needed
 
