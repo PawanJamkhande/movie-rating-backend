21c2e0345ed8278cb85596deb995c42a63be638d
Pawan JR. <pawanjamkhande001@gmail.com>
2026-08-05 00:00:09 +0530

    Update MovieService.java

--- movie-rating-backend/src/main/java/com/movierating/service/MovieService.java
+++ movie-rating-backend/src/main/java/com/movierating/service/MovieService.java
@@ -1,3 +1,10 @@
+/* Services are the managersof this project 
+Service Package handles :
+whether a user can register?
+whether an email already exists?
+whether a password should be encrypted?
+whether a login is valid? */
+
 package com.movierating.service;
 
 import java.util.List;
